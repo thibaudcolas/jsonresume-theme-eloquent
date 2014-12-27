62a83cc9ee9175fda25403dedaa05cca0731c37e
Thibaud Colas <thibaudcolas@gmail.com>
2014-12-28 02:07:52 +1300

    Add helpers for dates, phones, mails

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var Handlebars = require('handlebars');
+var moment = require('moment');
 
 var render = function(resume) {
   var css = fs.readFileSync(__dirname + '/style.css', 'utf-8');
@@ -13,8 +14,27 @@ var render = function(resume) {
 };
 
 Handlebars.registerHelper('nl2br', function(value) {
-    value = (value || '').replace(/\n/g, '<br>');
-    return new Handlebars.SafeString(value);
+    value = value || '';
+    var html = (value).replace(/\n/g, '<br>');
+    return new Handlebars.SafeString(html);
+});
+
+Handlebars.registerHelper('formatMMMMYYYY', function(value) {
+    value = value || '';
+    var html = moment(new Date((value))).format('MMMM YYYY');
+    return new Handlebars.SafeString(html);
+});
+
+Handlebars.registerHelper('formatDDMMYYYY', function(value) {
+    value = value || '';
+    var html = moment(new Date((value))).format('DD/MM/YYYY');
+    return new Handlebars.SafeString(html);
+});
+
+Handlebars.registerHelper('sanitizePhone', function(value) {
+    value = value || '';
+    var html = (value).replace(/[-\s]/g, '');
+    return new Handlebars.SafeString(html);
 });
 
 module.exports = {
