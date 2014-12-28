d47204c1b284d7ddfab9efacf5d2d3820f5675bf
Thibaud Colas <thibaudcolas@gmail.com>
2014-12-29 00:39:37 +1300

    Add a dev server w/ livereload to facilitate theme development

---
+++ server.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var path = require('path');
+var browserSync = require('browser-sync');
+var schema = require('resume-schema');
+var theme = require('./index.js');
+
+// resume is either read from a local file, or an example.
+var resume = (function() {
+  try {
+    var localResume = fs.readFileSync(path.join(__dirname, '/resume.json'), 'utf-8');
+    return JSON.parse(localResume);
+  }
+  catch (e) {
+    return schema.resumeJson;
+  }
+})();
+
+// BrowserSync watches those two files, nodemon takes care of the others.
+browserSync.init({
+  files: ['resume.template', 'resume.json'],
+  server: {
+    baseDir: './',
+    middleware: function (req, res, next) {
+      res.end(theme.render(resume));
+    }
+  },
+  injectChanges: false,
+  port: 4000,
+  open: false,
+  notify: false
+});
+
