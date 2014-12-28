8f33ca503c3e2bd41bf51010db7852cff823ffb4
Thibaud Colas <thibaudcolas@gmail.com>
2014-12-28 21:36:44 +1300

    Add separate helpers generation

--- index.js
+++ index.js
@@ -1,53 +1,18 @@
 var fs = require('fs');
 var Handlebars = require('handlebars');
-var moment = require('moment');
 
 var partials = require('./lib/partials');
+var helpers = require('./lib/helpers');
 
 Handlebars.registerPartial(partials);
-
-var render = function(resume) {
-  var template = fs.readFileSync(__dirname + '/resume.template', 'utf-8');
-
-  // TODO: Inline helper
-
-  return Handlebars.compile(template)({
-    resume: resume
-  });
-};
-
-
-
-Handlebars.registerHelper('nl2br', function(value) {
-  value = value || '';
-  var html = (value).replace(/\n/g, '<br>');
-  return new Handlebars.SafeString(html);
-});
-
-Handlebars.registerHelper('formatMMMMYYYY', function(value) {
-  value = value || '';
-  var html = moment(new Date((value))).format('MMMM YYYY');
-  return new Handlebars.SafeString(html);
-});
-
-Handlebars.registerHelper('formatMMMDDYYYY', function(value) {
-  value = value || '';
-  var html = moment(new Date((value))).format('MMM DD, YYYY');
-  return new Handlebars.SafeString(html);
-});
-
-Handlebars.registerHelper('sanitizePhone', function(value) {
-  value = value || '';
-  var html = (value).replace(/[-\s]/g, '');
-  return new Handlebars.SafeString(html);
-});
-
-Handlebars.registerHelper('sanitizeURL', function(value) {
-  value = value || '';
-  var html = (value).replace(/http?s:\/\//g, '');
-  return new Handlebars.SafeString(html);
-});
+Handlebars.registerHelper(helpers);
 
 module.exports = {
-  render: render
+  render: function(resume) {
+    var template = fs.readFileSync(__dirname + '/resume.template', 'utf-8');
+
+    return Handlebars.compile(template)({
+      resume: resume
+    });
+  }
 };
