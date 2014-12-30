ff9be4dfd5d48c020a19396d179c39e2d416afaf
Thibaud Colas <thibaudcolas@gmail.com>
2014-12-30 23:46:00 +1300

    Add custom download name

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,15 @@
-(function(document, domready, saveAs, smoothScroll) {
+(function(document, domready, saveAs, smoothScroll, downloadName) {
   'use strict';
 
   domready(function() {
 
     document.getElementById('js-download').addEventListener('click', function () {
       var blob = new Blob([document.documentElement.outerHTML], {type: 'text/html;charset=utf-8'});
-      saveAs(blob, 'resume.html');
+      saveAs(blob, downloadName);
     });
 
     smoothScroll.init({
       speed: 300
     });
   });
-})(document, window.domready, window.saveAs, window.smoothScroll);
+})(document, window.domready, window.saveAs, window.smoothScroll, window.downloadName);
