bc7d578ed48a5c5bd1320c55a8eada1a3a75a929
Thibaud Colas <thibaudcolas@gmail.com>
2015-01-05 21:46:31 +1300

    Remove domReady dependency

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,19 +1,17 @@
-(function(document, domready, saveAs, smoothScroll, downloadName) {
+(function(document, saveAs, smoothScroll, downloadName) {
   'use strict';
 
-  domready(function() {
-    if(window.location.protocol.indexOf('http') !== -1) {
-      // Created before download in order to be JS-modifications-free.
-      var blob = new Blob(['<!doctype html>' + document.documentElement.outerHTML], {type: 'text/html;charset=utf-8'});
-      document.getElementsByClassName('js-download-trigger')[0].style.display = 'block';
-    }
+  if(window.location.protocol.indexOf('http') !== -1) {
+    // Created before download in order to be JS-modifications-free.
+    var blob = new Blob(['<!doctype html>' + document.documentElement.outerHTML], {type: 'text/html;charset=utf-8'});
+    document.getElementsByClassName('js-download-trigger')[0].style.display = 'block';
+  }
 
-    document.getElementById('js-download').addEventListener('click', function () {
-      saveAs(blob, downloadName);
-    });
-
-    smoothScroll.init({
-      speed: 300
-    });
+  document.getElementById('js-download').addEventListener('click', function () {
+    saveAs(blob, downloadName);
   });
-})(document, window.domready, window.saveAs, window.smoothScroll, window.downloadName);
+
+  smoothScroll.init({
+    speed: 300
+  });
+})(document, window.saveAs, window.smoothScroll, window.downloadName);
