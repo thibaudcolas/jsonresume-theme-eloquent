674452e37dc30389046b1d3f99a0c20062bf7100
Thibaud Colas <thibaudcolas@gmail.com>
2015-01-07 22:49:34 +1300

    Move click handler inside the right block

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,15 @@
 (function(document, saveAs, smoothScroll, downloadName) {
   'use strict';
 
-  if(window.location.protocol.indexOf('http') !== -1) {
+  if (window.location.protocol.indexOf('http') !== -1) {
     // Created before download in order to be JS-modifications-free.
     var blob = new Blob(['<!doctype html>' + document.documentElement.outerHTML], {type: 'text/html;charset=utf-8'});
     document.getElementsByClassName('js-download-trigger')[0].style.display = 'block';
-  }
 
-  document.getElementById('js-download').addEventListener('click', function () {
-    saveAs(blob, downloadName);
-  });
+    document.getElementById('js-download').addEventListener('click', function () {
+      saveAs(blob, downloadName);
+    });
+  }
 
   smoothScroll.init({
     speed: 300
