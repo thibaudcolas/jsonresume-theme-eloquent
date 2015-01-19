977ad1f634cc27b4b9e2d4d9c274f4abecb60562
Thibaud Colas <thibaudcolas@gmail.com>
2015-01-19 23:12:45 +1300

    Add another way to obfuscate text, friendlier to JS-less envs

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,18 +11,11 @@
     });
   }
 
-  var mailText = document.querySelectorAll('[data-scrambled-email-text]')[0];
   var mailtoLink = document.querySelectorAll('[data-scrambled-email-link]')[0];
-  var decodedMail = emailScramble.decode(mailtoLink.href.replace('mailto:', ''));
-  mailtoLink.href = 'mailto:' + decodedMail;
-  mailText.innerHTML = decodedMail;
-  mailText.setAttribute('itemprop', 'email');
+  mailtoLink.href = 'mailto:' + emailScramble.decode(mailtoLink.href.replace('mailto:', ''));
 
-  var phoneText = document.querySelectorAll('[data-scrambled-phone-text]')[0];
   var phoneLink = document.querySelectorAll('[data-scrambled-phone-link]')[0];
   phoneLink.href = 'tel:' + emailScramble.decode(phoneLink.href.replace('tel:', ''));
-  phoneText.innerHTML = emailScramble.decode(phoneText.innerHTML);
-  phoneText.setAttribute('itemprop', 'telephone');
 
   smoothScroll.init({
     speed: 300
