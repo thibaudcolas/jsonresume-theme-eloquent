836f9f6b3c53fbb5ae3e7460ed588586f55d29e7
Thibaud Colas <thibaudcolas@gmail.com>
2015-10-29 10:32:01 +0200

    Remove unnecessary test statements

--- test/helpers.spec.js
+++ test/helpers.spec.js
@@ -4,13 +4,11 @@ import helpers from '../lib/helpers';
 
 describe('Helpers', () => {
     it('should be an object', () => {
-        expect(helpers).to.exist;
         expect(helpers).to.be.an('object');
     });
 
     describe('format', () => {
         it('should expose a "MMMMYYYY" format', () => {
-            expect(helpers.formatMMMMYYYY).to.exist;
             expect(helpers.formatMMMMYYYY).to.be.a('function');
         });
 
@@ -19,7 +17,6 @@ describe('Helpers', () => {
         });
 
         it('should expose a "MMMDDYYYY" format', () => {
-            expect(helpers.formatMMMDDYYYY).to.exist;
             expect(helpers.formatMMMDDYYYY).to.be.a('function');
         });
 
@@ -28,7 +25,6 @@ describe('Helpers', () => {
         });
 
         it('should expose a "YYYY" format', () => {
-            expect(helpers.formatYYYY).to.exist;
             expect(helpers.formatYYYY).to.be.a('function');
         });
 
