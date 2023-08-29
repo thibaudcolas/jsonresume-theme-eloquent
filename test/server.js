8f6ed2852b748f005d4d8119b880754a87ca501c
Thibaud Colas <thibaudcolas@gmail.com>
2023-08-29 22:40:01 +0100

    Simplify resume code structure

--- test/server.js
+++ test/server.js
@@ -1,12 +1,12 @@
 var fs = require("node:fs/promises");
 var path = require("path");
 const http = require("http");
-var theme = require("./index.js");
+var theme = require("../lib/resume");
 
 const server = http.createServer(async (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   var localResume = await fs.readFile(
-    path.join(__dirname, "/test/resume.json"),
+    path.join(__dirname, "resume.json"),
     "utf-8"
   );
   res.end(theme.render(JSON.parse(localResume)));
